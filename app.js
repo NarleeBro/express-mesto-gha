abe0dcf5263a83b30d0a457bd51b3f761a189320
NarleeBro <linarpr@gmail.com>
2023-08-04 20:48:10 +0300

    test2

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/testdb" } =
-  process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/testdb' } = process.env;
 /* const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env; */
 
 const app = express();
@@ -18,12 +17,12 @@ mongoose.connect(DB_URL, {
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "64cd37826fd8dd0287c8be73",
+    _id: '64cd37826fd8dd0287c8be73',
   };
 
   next();
 });
 
-app.use("/users", require("./routes/users"));
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT);
--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,9 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() =>
-      res.status(500).send({ message: "На сервере произошла ошибка" })
-    );
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.getUserById = (req, res) => {
@@ -15,18 +13,16 @@ module.exports.getUserById = (req, res) => {
         if (!user) {
           res
             .status(404)
-            .send({ message: "Пользователь по указанному _id не найден" });
+            .send({ message: 'Пользователь по указанному _id не найден' });
           return;
         }
         res.send(user);
       })
-      .catch(() =>
-        res
-          .status(404)
-          .send({ message: "Пользователь по указанному _id не найден" })
-      );
+      .catch(() => res
+        .status(404)
+        .send({ message: 'Пользователь по указанному _id не найден' }));
   } else {
-    res.status(400).send({ message: "Некорректный _id" });
+    res.status(400).send({ message: 'Некорректный _id' });
   }
 };
 
@@ -35,10 +31,10 @@ module.exports.addUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
     .catch((error) => {
-      if (error.name === "ValidationError") {
+      if (error.name === 'ValidationError') {
         res.status(400).send({ message: error.message });
       } else {
-        res.status(500).send({ message: "На сервере произошла ошибка" });
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
     });
 };
@@ -49,20 +45,20 @@ module.exports.editUserData = (req, res) => {
     User.findByIdAndUpdate(
       req.user._id,
       { name, about },
-      { new: "true", runValidators: true }
+      { new: 'true', runValidators: true },
     )
       .then((user) => res.send(user))
       .catch((error) => {
-        if (error.name === "ValidationError") {
+        if (error.name === 'ValidationError') {
           res.status(400).send({ message: error.message });
         } else {
           res
             .status(404)
-            .send({ message: "Пользователь по указанному _id не найден" });
+            .send({ message: 'Пользователь по указанному _id не найден' });
         }
       });
   } else {
-    res.status(500).send({ message: "На сервере произошла ошибка" });
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
   }
 };
 
@@ -71,17 +67,17 @@ module.exports.editUserAvatar = (req, res) => {
     User.findByIdAndUpdate(
       req.user._id,
       { avatar: req.body.avatar },
-      { new: "true", runValidators: true }
+      { new: 'true', runValidators: true },
     )
       .then((user) => res.send(user))
       .catch((error) => {
-        if (error.name === "ValidationError") {
+        if (error.name === 'ValidationError') {
           res.status(400).send({ message: error.message });
         } else {
-          res.status(404).send({ message: "Пользователь по указанному _id не найден" });
+          res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
         }
       });
   } else {
-    res.status(500).send({ message: "На сервере произошла ошибка" });
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
   }
 };
