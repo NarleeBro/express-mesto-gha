5a5274c3fbbe73df7395098c36885a4ef0bfce80
NarleeBro <linarpr@gmail.com>
2023-08-27 15:29:13 +0300

    test143

--- errors/BadRequestError.js
+++ errors/BadRequestError.js
@@ -1,9 +1,9 @@
-const { HTTP_STATUS_BAD_REQUEST } = require('http2').constants;
+/* const { HTTP_STATUS_BAD_REQUEST } = require('http2').constants; */
 
 module.exports = class BadRequestError extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = HTTP_STATUS_BAD_REQUEST;
-    /* this.statusCode = 400; */
+    /* this.statusCode = HTTP_STATUS_BAD_REQUEST; */
+    this.statusCode = 400;
   }
 };
